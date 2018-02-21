0c6fb9c543f80c0c94f0e8654f95f1477dea473d
Marcin Swornowski <swor71@gmail.com>
2018-02-21 16:27:22 +0100

    Add Exercise 16.4-Title-component

---
+++ src/components/Title.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class Title extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>App Title Here</h1>
+                <h3>Items to do:
+                    <span></span>
+                </h3>
+            </div>
+        )
+    }
+}
+
+export default Title;
